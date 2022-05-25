ff90aa1b1ef9308c32e3ea9a7945f85e3230b948
Qamariddin Yaqubov <106054129+Qamariddin2000@users.noreply.github.com>
2022-05-25 12:49:21 +0500

    Create 14-dars.Lug'at-2.py
    2-vazifa

---
+++ 14-dars.Lug'at-2.py
@@ -0,0 +1,10 @@
+taomlar={
+    'ali':'osh',
+    'vali':'shashlik',
+    'hasan':"lag'mon",
+    'husan':'mastava',
+    'olim':'somsa'
+    }
+print(f"Alining sevimli taomi {taomlar['ali']}")
+print(f"Valining sevimli taomi {taomlar['vali']}")
+print(f"Hasanning sevimli taomi {taomlar['hasan']}")
