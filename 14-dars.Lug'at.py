63305a40af10d7c35242259bed3c053ff24a0d6d
Qamariddin Yaqubov <106054129+Qamariddin2000@users.noreply.github.com>
2022-05-25 12:34:19 +0500

    Create 14-dars.Lug'at.py
    Yangi fayl yuklandi

---
+++ 14-dars.Lug'at.py
@@ -0,0 +1,2 @@
+otam={'ism':'tangirov muso', 'tyil':1970, 'viloyat':'qashqadaryo'}
+print(f"Otamning ismi {otam['ism'].title()}, {otam['tyil']}-yilda, {otam['viloyat'].title()} viloyatida tug'ilgan")
