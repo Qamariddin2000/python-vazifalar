bfd4b00f951ba2e0a98998b2943550b2c5274e22
Qamariddin Yaqubov <106054129+Qamariddin2000@users.noreply.github.com>
2022-06-09 19:11:24 +0500

    Create 16-dars.3.py

---
+++ 16-dars.3.py
@@ -0,0 +1,10 @@
+kinolar={
+    'ali':['Teminator','Rambo',"Titanic"],
+    'vali':['Tenet','Inception','Interstellar'],
+    'hasan':['Abdullajon','Bomba','Shaytanat'],
+    'Husan':['Mahallada duv-duv gap', 'John Wick','Transfomers']
+}
+for ismlar, movies in kinolar.items():
+    print(f"\n{ismlar.title()}ning sevimli kinlari: ")
+    for kino in movies:
+        print(kino)
