8d49f139787ab3720bad7ccf9de0749235c14725
Qamariddin Yaqubov <106054129+Qamariddin2000@users.noreply.github.com>
2022-08-04 13:15:47 +0500

    20-dars

---
+++ 21-dars.2.py
@@ -0,0 +1,10 @@
+def bahola(ismlar):
+    baholar={}
+    for ism in ismlar:
+        baho=input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+        baholar[ism]=baho
+    return baholar
+talabalar = ['ali', 'vali', 'hasan', 'husan']
+baholar=bahola(talabalar)
+print(baholar)
+print(talabalar)
